8a6fd10f9c193cb4f017346c970ce4fde78bbdd3
henriq20 <henrique-ferreira20@hotmail.com>
2022-10-17 13:52:41 -0300

    feat: add the board and the piece interface

---
+++ src/pieces/piece.ts
@@ -0,0 +1,15 @@
+import ChessBoard from '../board.js';
+import Square from '../square';
+
+export type ChessPieceName = 'king' | 'queen' | 'rook' | 'bishop' | 'knight' | 'pawn';
+
+export type ChessPieceColor = 'black' | 'white';
+
+export interface ChessPiece {
+    name: ChessPieceName;
+    color: ChessPieceColor;
+    letter: string;
+    moves?: number;
+    board: ChessBoard | null;
+    square: Square | null
+}
---
+++ src/square.ts
@@ -0,0 +1,15 @@
+import { ChessPiece } from './pieces/piece.js';
+
+export default class Square {
+	readonly name: string;
+	readonly x: number;
+	readonly y: number;
+	piece: ChessPiece | null;
+
+	constructor(name: string, x: number, y: number, piece?: ChessPiece) {
+		this.name = name;
+		this.x = x;
+		this.y = y;
+		this.piece = piece ?? null;
+	}
+}
