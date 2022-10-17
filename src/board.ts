be1398f47d465f4431252bbd5ea51e606be2b711
henriq20 <henrique-ferreira20@hotmail.com>
2022-10-17 13:58:05 -0300

    test: add tests for the board

--- src/board.ts
+++ src/board.ts
@@ -1,26 +1,26 @@
 import Square from './square.js';
 
 export default class ChessBoard {
-    readonly size: number;
-    _board: Square[][];
+	readonly size: number;
+	_board: Square[][];
 
-    constructor() {
-        this.size = 8;
-        this._board = this.clear();
-    }
+	constructor() {
+		this.size = 8;
+		this._board = this.clear();
+	}
 
-    clear() {
-        this._board = [];
+	clear() {
+		this._board = [];
 
-        for (let row = 0; row < this.size; row++) {
-            const rank = String.fromCharCode('a'.charCodeAt(0) + row);
-            this._board[row] = [];
+		for (let row = 0; row < this.size; row++) {
+			const rank = String.fromCharCode('a'.charCodeAt(0) + row);
+			this._board[row] = [];
 
-            for (let column = 0; column < this.size; column++) {
-                this._board[row].push(new Square(rank + (column + 1).toString(), row, column));
-            }
-        }
+			for (let column = 0; column < this.size; column++) {
+				this._board[row].push(new Square(rank + (column + 1).toString(), row, column));
+			}
+		}
 
-        return this._board;
-    }
+		return this._board;
+	}
 }
---
+++ tests/board.test.ts
@@ -0,0 +1,58 @@
+import Square from '../src/square';
+import ChessBoard from '../src/board';
+
+it('should create an empty board', () => {
+    const board = new ChessBoard();
+
+    expect(board.size).toBe(8);
+    expect(board._board).toHaveLength(8);
+    expect(board._board.every(row => row.length === 8)).toBe(true);
+});
+
+it('should fill the board with squares', () => {
+    const board = new ChessBoard();
+
+    expect(board._board.every(row => row.every(s => s instanceof Square))).toBe(true);
+});
+
+it('should name each square based on its position', () => {
+    const board = new ChessBoard();
+
+    const squares = [
+        [ 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8' ],
+        [ 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8' ],
+        [ 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8' ],
+        [ 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8' ],
+        [ 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8' ],
+        [ 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8' ],
+        [ 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8' ],
+        [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8' ]
+    ];
+
+    for (let i = 0; i < squares.length; i++) {
+        const row = squares[i];
+
+        expect(board._board[i].map(s => s.name)).toStrictEqual(row);
+    }
+});
+
+it('should set each square row and column based on its position', () => {
+    const board = new ChessBoard();
+
+    const squares = [
+        [ [0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7] ],
+        [ [1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7] ],
+        [ [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7] ],
+        [ [3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7] ],
+        [ [4, 0], [4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7] ],
+        [ [5, 0], [5, 1], [5, 2], [5, 3], [5, 4], [5, 5], [5, 6], [5, 7] ],
+        [ [6, 0], [6, 1], [6, 2], [6, 3], [6, 4], [6, 5], [6, 6], [6, 7] ],
+        [ [7, 0], [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6], [7, 7] ]
+    ];
+
+    for (let i = 0; i < squares.length; i++) {
+        const row = squares[i];
+
+        expect(board._board[i].map(s => [ s.x, s.y ])).toStrictEqual(row);
+    }
+});
