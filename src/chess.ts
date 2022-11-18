cc429ce116eab48e831f5679558edeee0bb1caad
henriq20 <henrique-ferreira20@hotmail.com>
2022-11-18 15:05:01 -0300

    feat: add function to setup board

--- src/chess.ts
+++ src/chess.ts
@@ -1,24 +1,58 @@
 import ChessBoard from './board';
 import { ChessPieceLetter } from './factory';
 import ChessPiece from './pieces/piece';
-import { ChessPosition } from './position';
+import { ArrayPosition, ChessPosition } from './position';
 import createPiece from './factory';
 import { toArrayPosition } from './position';
+
+export type SetupFn = (place: (pieceName: ChessPieceLetter, position: ChessPosition | ArrayPosition) => ChessPiece | false) => void;
 
 export default class Chess {
     board: ChessBoard;
     white: ChessPiece[];
     black: ChessPiece[];
 
-    constructor() {
+    constructor(setupFn?: SetupFn) {
         this.board = new ChessBoard();
         this.white = [];
         this.black = [];
+        this.setup(setupFn);
     }
 
-    place(pieceName: ChessPieceLetter, position: ChessPosition): ChessPiece | false {
+    setup(fn?: SetupFn) {
+        if (typeof fn === 'function') {
+            return fn(this.place.bind(this));
+        }
+
+        // Place pawns
+        for (let column = 0; column < this.board.size; column++) {
+
+            this.place('p', [ 1, column ]);
+            this.place('P', [ 6, column ]);
+        }
+
+        this.place('r', [ 0, 0 ]);
+        this.place('n', [ 0, 1 ]);
+        this.place('b', [ 0, 2 ]);
+        this.place('q', [ 0, 3 ]);
+        this.place('k', [ 0, 4 ]);
+        this.place('b', [ 0, 5 ]);
+        this.place('n', [ 0, 6 ]);
+        this.place('r', [ 0, 7 ]);
+
+        this.place('R', [ 7, 0 ]);
+        this.place('N', [ 7, 1 ]);
+        this.place('B', [ 7, 2 ]);
+        this.place('Q', [ 7, 3 ]);
+        this.place('K', [ 7, 4 ]);
+        this.place('B', [ 7, 5 ]);
+        this.place('N', [ 7, 6 ]);
+        this.place('R', [ 7, 7 ]);
+    }
+
+    place(pieceName: ChessPieceLetter, position: ChessPosition | ArrayPosition): ChessPiece | false {
         const piece = createPiece(pieceName);
-        const [ row, column ] = toArrayPosition(position);
+        const [ row, column ] = typeof position === 'string' ? toArrayPosition(position) : position;
 
         const result = this.board.place(row, column, piece);
 
--- tests/chess.test.ts
+++ tests/chess.test.ts
@@ -1,9 +1,14 @@
 import Chess from '../src/chess';
+import Bishop from '../src/pieces/bishop';
+import King from '../src/pieces/king';
+import Knight from '../src/pieces/knight';
 import Pawn from '../src/pieces/pawn';
+import Queen from '../src/pieces/queen';
+import Rook from '../src/pieces/rook';
 
 describe('place', () => {
     it('should place a piece on the specified position', () => {
-        const chess = new Chess();
+        const chess = new Chess(() => {});
 
         chess.place('p', 'a1');
 
@@ -14,7 +19,7 @@ describe('place', () => {
     });
 
     it('should return the piece created', () => {
-        const chess = new Chess();
+        const chess = new Chess(() => {});
 
         const piece = chess.place('p', 'a1');
 
@@ -22,7 +27,7 @@ describe('place', () => {
     });
 
     it('should return false when the piece was not added', () => {
-        const chess = new Chess();
+        const chess = new Chess(() => {});
 
         const piece = chess.place('p', 'a9');
 
@@ -30,13 +35,72 @@ describe('place', () => {
     });
 
     it('should add the piece to the pieces array', () => {
-        const chess = new Chess();
+        const chess = new Chess(() => {});
 
         chess.place('p', 'a2');
         chess.place('P', 'a7');
 
         expect(chess.white.length).toBe(1);
         expect(chess.black.length).toBe(1);
+        expect(chess.white[0].color).toBe('white');
+        expect(chess.black[0].color).toBe('black');
+    });
+
+    it('should add a piece by passing row and column', () => {
+        const chess = new Chess(() => {});
+
+        chess.place('p', [ 0, 0 ]);
+
+        expect(chess.white.length).toBe(1);
+        expect(chess.board.get(0, 0)?.piece).toBeInstanceOf(Pawn);
+    });
+});
+
+describe('setup', () => {
+    it('should place the initial pieces on the board', () => {
+        const chess = new Chess();
+
+        expect(chess.white.length).toBe(16);
+        expect(chess.black.length).toBe(16);
+
+        for (let column = 0; column < chess.board.size; column++) {
+            const white = chess.board.get(1, column);
+            const black = chess.board.get(6, column);
+
+            expect(white?.piece).toBeInstanceOf(Pawn);
+            expect(black?.piece).toBeInstanceOf(Pawn);
+            expect(white?.piece?.color).toBe('white');
+            expect(black?.piece?.color).toBe('black');
+        }
+
+        expect(chess.board.get(0, 0)?.piece).toBeInstanceOf(Rook);
+        expect(chess.board.get(0, 1)?.piece).toBeInstanceOf(Knight);
+        expect(chess.board.get(0, 2)?.piece).toBeInstanceOf(Bishop);
+        expect(chess.board.get(0, 3)?.piece).toBeInstanceOf(Queen);
+        expect(chess.board.get(0, 4)?.piece).toBeInstanceOf(King);
+        expect(chess.board.get(0, 5)?.piece).toBeInstanceOf(Bishop);
+        expect(chess.board.get(0, 6)?.piece).toBeInstanceOf(Knight);
+        expect(chess.board.get(0, 7)?.piece).toBeInstanceOf(Rook);
+
+        expect(chess.board.get(7, 0)?.piece).toBeInstanceOf(Rook);
+        expect(chess.board.get(7, 1)?.piece).toBeInstanceOf(Knight);
+        expect(chess.board.get(7, 2)?.piece).toBeInstanceOf(Bishop);
+        expect(chess.board.get(7, 3)?.piece).toBeInstanceOf(Queen);
+        expect(chess.board.get(7, 4)?.piece).toBeInstanceOf(King);
+        expect(chess.board.get(7, 5)?.piece).toBeInstanceOf(Bishop);
+        expect(chess.board.get(7, 6)?.piece).toBeInstanceOf(Knight);
+        expect(chess.board.get(7, 7)?.piece).toBeInstanceOf(Rook);
+    });
+
+    it('should work with a custom function', () => {
+        const chess = new Chess(place => {
+            place('p', 'a2');
+            place('P', 'a7');
+        });
+
+        expect(chess.white.length).toBe(1);
+        expect(chess.black.length).toBe(1);
+
         expect(chess.white[0].color).toBe('white');
         expect(chess.black[0].color).toBe('black');
     });
