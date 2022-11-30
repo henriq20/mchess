1df4a9948ce5afd5bbd70a84899f262bf8318958
henriq20 <henrique-ferreira20@hotmail.com>
2022-11-29 22:35:33 -0300

    refactor: move chess position to piece

--- src/pieces/piece.ts
+++ src/pieces/piece.ts
@@ -1,6 +1,3 @@
-import Chess from '../chess.js';
-import { ChessPosition } from '../board/position.js';
-
 export type ChessPieceType = 'k' | 'q' | 'r' | 'b' | 'n' | 'p';
 
 export type ChessPieceColor = 'black' | 'white';
@@ -9,7 +6,17 @@ export type ChessPieceSymbol =
 	'k' | 'q' | 'r' | 'b' | 'n' | 'p' |
 	'K' | 'Q' | 'R' | 'B' | 'N' | 'P';
 
-export default abstract class ChessPiece {
+export type ChessPosition =
+'a8' | 'b8' | 'c8' | 'd8' | 'e8' | 'f8' | 'g8' | 'h8' |
+'a7' | 'b7' | 'c7' | 'd7' | 'e7' | 'f7' | 'g7' | 'h7' |
+'a6' | 'b6' | 'c6' | 'd6' | 'e6' | 'f6' | 'g6' | 'h6' |
+'a5' | 'b5' | 'c5' | 'd5' | 'e5' | 'f5' | 'g5' | 'h5' |
+'a4' | 'b4' | 'c4' | 'd4' | 'e4' | 'f4' | 'g4' | 'h4' |
+'a3' | 'b3' | 'c3' | 'd3' | 'e3' | 'f3' | 'g3' | 'h3' |
+'a2' | 'b2' | 'c2' | 'd2' | 'e2' | 'f2' | 'g2' | 'h2' |
+'a1' | 'b1' | 'c1' | 'd1' | 'e1' | 'f1' | 'g1' | 'h1' | '-';
+
+export default class ChessPiece {
 	type: ChessPieceType;
 	color: ChessPieceColor;
 	square: ChessPosition;
@@ -19,8 +26,6 @@ export default abstract class ChessPiece {
 		this.color = color;
 		this.square = square || '-';
 	}
-
-	abstract possibleMoves(chess: Chess): ChessPosition[];
 
 	get symbol() {
 		return this.color === 'white' ? this.type.toUpperCase() : this.type;
