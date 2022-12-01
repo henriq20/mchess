7b50c53e2acccb8105fecff323fee0a4a8d2ebb7
henriq20 <henrique-ferreira20@hotmail.com>
2022-12-01 08:32:50 -0300

    refactor: remove unused file

--- src/utils.ts
+++
@@ -1,4 +0,0 @@
-export function pad(str: string, char: string, maxLength: number): string {
-	return str.padStart(str.length + Math.floor((maxLength - str.length) / 2), char)
-		.padEnd(maxLength, char);
-}
