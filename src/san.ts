b87d3c0858607aa26a560cb575bfd4bf24a889ad
henriq20 <henrique-ferreira20@hotmail.com>
2022-12-04 19:18:55 -0300

    refactor: use const instead of let

--- src/san.ts
+++ src/san.ts
@@ -5,80 +5,74 @@ import generateMoves, { PseudoMove } from './pieces/moves';
 import { CHESS_POSITIONS, SQUARE_MAP } from './board/board';
 
 const CASTLING_SQUARES: { [key: string]: { kingside: number, queenside: number } } = {
-    white: {
-        kingside: SQUARE_MAP.g1,
-        queenside: SQUARE_MAP.c1
-    },
-    black: {
-        kingside: SQUARE_MAP.g8,
-        queenside: SQUARE_MAP.c8
-    }
-}
+	white: {
+		kingside: SQUARE_MAP.g1,
+		queenside: SQUARE_MAP.c1
+	},
+	black: {
+		kingside: SQUARE_MAP.g8,
+		queenside: SQUARE_MAP.c8
+	}
+};
 
 export function parse(chess: Chess, san: string): PseudoMove & { promoteTo?: ChessPieceType } | false {
-    san = stripDecorators(san);
+	san = stripDecorators(san);
 
-    if (san.startsWith('O-O') || san.startsWith('o-o')) {
-        const king = chess.kings[chess.turn];
+	if (san.startsWith('O-O') || san.startsWith('o-o')) {
+		const king = chess.kings[chess.turn];
 
-        if (!king) {
-            return false;
-        }
+		if (!king) {
+			return false;
+		}
 
-        const isKingsideCastle = san.length === 3;
-        const to = isKingsideCastle ?
-            CHESS_POSITIONS[CASTLING_SQUARES[chess.turn].kingside] :
-            CHESS_POSITIONS[CASTLING_SQUARES[chess.turn].queenside]
+		const isKingsideCastle = san.length === 3;
+		const to = isKingsideCastle ?
+			CHESS_POSITIONS[CASTLING_SQUARES[chess.turn].kingside] :
+			CHESS_POSITIONS[CASTLING_SQUARES[chess.turn].queenside];
 
-        return {
-            from: king.square,
-            to,
-            type: isKingsideCastle ? MoveType.KINGSIDE_CASTLE : MoveType.QUEENSIDE_CASTLE
-        };
-    }
+		return {
+			from: king.square,
+			to,
+			type: isKingsideCastle ? MoveType.KINGSIDE_CASTLE : MoveType.QUEENSIDE_CASTLE
+		};
+	}
 
-    const matches = /([nbqkrNBQKR])?([a-h1-8][1-8]?)?x?([a-h][1-8])([nbqrNBQR])?/g.exec(san);
+	const matches = /([nbqkrNBQKR])?([a-h1-8][1-8]?)?x?([a-h][1-8])([nbqrNBQR])?/g.exec(san);
 
-    if (!matches) {
-        return false;
-    }
+	if (!matches) {
+		return false;
+	}
 
-    let [
-        pieceType,
-        disambiguator,
-        to,
-        promoteTo
-    ] = matches.slice(1);
+	const [
+		pieceType,
+		disambiguator,
+		to,
+		promoteTo
+	] = matches.slice(1);
 
-    if (!to) {
-        return false;
-    }
+	if (!to) {
+		return false;
+	}
 
-    if (!pieceType) {
-        pieceType = 'p';
-    }
+	const moves = generateMoves(chess, { piece: (pieceType ?? 'p').toLowerCase() as ChessPieceType });
+	const possibleMoves = moves.filter(m => m.to === to);
 
-    pieceType = pieceType.toLowerCase() as ChessPieceType;
+	if (!possibleMoves.length) {
+		return false;
+	}
 
-    const moves = generateMoves(chess, { piece: pieceType as ChessPieceType });
-    const possibleMoves = moves.filter(m => m.to === to);
+	// Ambiguious moves
+	if (possibleMoves.length > 1) {
+		// Takes the move where the 'from' position contains the disambiguator
+		return possibleMoves.find(m => m.from.indexOf(disambiguator) !== -1) || false;
+	}
 
-    if (!possibleMoves.length) {
-        return false;
-    }
-
-    // Ambiguious moves
-    if (possibleMoves.length > 1) {
-        // Takes the move where the 'from' position contains the disambiguator
-        return possibleMoves.find(m => m.from.indexOf(disambiguator) !== -1) || false;
-    }
-
-    return !promoteTo ? possibleMoves[0] : {
-        ...possibleMoves[0],
-        promoteTo: promoteTo.toLowerCase() as ChessPieceType
-    };
+	return !promoteTo ? possibleMoves[0] : {
+		...possibleMoves[0],
+		promoteTo: promoteTo.toLowerCase() as ChessPieceType
+	};
 }
 
 function stripDecorators(san: string) {
-    return san.replace(/[=+#?!]+/, '');
+	return san.replace(/[=+#?!]+/, '');
 }
