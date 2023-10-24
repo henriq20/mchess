2bfc5a4053ab6dc1d5d74733cb6bb22528e78fdc
henriq20 <henrique-ferreira20@hotmail.com>
2023-10-24 11:02:19 -0300

    fix: terser import

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import dts from 'rollup-plugin-dts';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import packageJson from './package.json' assert { type: 'json' };
 
